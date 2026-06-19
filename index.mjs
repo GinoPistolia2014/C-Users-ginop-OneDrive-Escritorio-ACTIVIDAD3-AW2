b5fde9c28d4f54a9daf9e1dec4dfba3afcd3bb66
ginop <ginopistolia2014@gmail.com>
2026-06-18 21:34:17 -0300

    Actividad 3 finalizada

--- index.mjs
+++ index.mjs
@@ -1,23 +1,48 @@
-import http from 'node:http'
-import fsp from 'node:fs/promises'
-import path from 'node:path'
-
-const app = http.createServer((peticion, respuesta) => {
-    if (peticion.method === 'GET') {
-        if (peticion.url === '/usuarios') {
-
-            
+import http from 'node:http';
+import fsp from 'node:fs/promises';
+import path from 'node:path';
 
 
-            respuesta.statusCode = 200
-            return respuesta.end('Estas en la ruta usuarios')
-        }
+const ARCHIVO_USUARIOS = path.join(process.cwd(), 'usuarios.json');
+
+
+async function obtenerYGuardarUsuarios() {
+    const respuestaApi = await fetch('https://api.escuelajs.co/api/v1/users');
+    
+    if (!respuestaApi.ok) {
+        throw new Error('No se pudo obtener respuesta de la API externa');
     }
+    
+    const datos = await respuestaApi.json();
+    await fsp.writeFile(ARCHIVO_USUARIOS, JSON.stringify(datos, null, 2), 'utf-8');
 
-    respuesta.statusCode = 404
-    respuesta.end('Ruta no encontrada')
-})
+    const contenidoArchivo = await fsp.readFile(ARCHIVO_USUARIOS, 'utf-8');
+    return contenidoArchivo;
+}
 
-app.listen(3000, () => {
-    console.log('servidor corriendo en http://localhost:4000')
-})
+const app = http.createServer(async (peticion, respuesta) => {
+    
+ 
+    respuesta.setHeader('Content-Type', 'application/json; charset=utf-8');
+
+    try {
+        if (peticion.method === 'GET' && peticion.url === '/usuarios') {
+            const datos = await obtenerYGuardarUsuarios();
+            
+            respuesta.statusCode = 200;
+            return respuesta.end(datos);
+        }
+        respuesta.statusCode = 404;
+        return respuesta.end(JSON.stringify({ mensaje: 'Recurso no encontrado' }));
+
+    } catch (error) {
+        console.error('Error:', error);
+        respuesta.statusCode = 500;
+        return respuesta.end(JSON.stringify({ mensaje: 'Error interno del servidor' }));
+    }
+});
+
+const PUERTO = 4000;
+app.listen(PUERTO, () => {
+    console.log(`Servidor corriendo en http://localhost:${PUERTO}`);
+});
